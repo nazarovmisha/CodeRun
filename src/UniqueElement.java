b4123d7e8ff6dab2a55e5e18a6ad321de2c40625
Nazarov Mikhail <nazarovmisha@inbox.ru>
2024-09-07 14:19:37 +0300

    UniqueElement

--- src/UniqueElement.java
+++ src/UniqueElement.java
@@ -4,8 +4,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 //https://coderun.yandex.ru/problem/exactly-one-occur?currentPage=1&groups=%D0%91%D0%B5%D0%BA%D0%B5%D0%BD%D0%B4&pageSize=10&rowNumber=2
 
@@ -16,14 +15,14 @@ public class UniqueElement {
         int index = Integer.parseInt(reader.readLine());
         String[] nums = reader.readLine().split(" ");
         reader.close();
-        Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < index; i++) {
-            set.add(Integer.parseInt(nums[i]));
+        Arrays.sort(nums);
+        Set<String> list=new HashSet<>(List.of(nums));
+        for (int i = 0; i < index-1; i++) {
+            if (nums[i].equals(nums[i+1])){
+                list.remove(nums[i]);
+            }
         }
-        int result=0;
-        int count=index-set.size();
-        result=index-count;
-
+        int result=list.size();
         String stringResult = Integer.toString(result);
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
         writer.write(stringResult);
