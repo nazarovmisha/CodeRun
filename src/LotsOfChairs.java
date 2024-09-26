f21cb2c3ca5546933593cf49fdcfc3a99178a99a
Nazarov Mikhail <nazarovmisha@inbox.ru>
2024-09-26 19:32:33 +0300

    FavoriteNumbers ClosedKey182

--- src/LotsOfChairs.java
+++ src/LotsOfChairs.java
@@ -10,10 +10,10 @@ public class LotsOfChairs {
         String salePriceString = reader.readLine();
         String purchasePriceString = reader.readLine();
         reader.close();
-        int[] sellersAndByers = Arrays.stream(byers.split(" ")).mapToInt(Integer::parseInt).toArray();
-        int[] salePrice = Arrays.stream(salePriceString.split(" ")).mapToInt(Integer::parseInt).toArray();
-        int[] purchasePrice = Arrays.stream(purchasePriceString.split(" ")).mapToInt(Integer::parseInt).toArray();
-        int sales;
+        long[] sellersAndByers = Arrays.stream(byers.split(" ")).mapToLong(Integer::parseInt).toArray();
+        long[] salePrice = Arrays.stream(salePriceString.split(" ")).mapToLong(Integer::parseInt).toArray();
+        long[] purchasePrice = Arrays.stream(purchasePriceString.split(" ")).mapToLong(Integer::parseInt).toArray();
+        long sales;
         if (sellersAndByers[0] < sellersAndByers[1]) {
             sales = sellersAndByers[0];
         } else {
@@ -21,12 +21,15 @@ public class LotsOfChairs {
         }
         Arrays.sort(salePrice);
         Arrays.sort(purchasePrice);
-        long result = 0;
-        for (int i = 0, j = sales-1; i < sales; i++, j--) {
-            if ((purchasePrice[j] - salePrice[i]) > 0) {
-                result += (purchasePrice[j] - salePrice[i]);
+        long result = 0l;
+        long a =0l;
+        long b =0l;
+        for (long i = 0l, j = sales-1l; i < sales; i++, j--) {
+           a+=salePrice[(int) i];
+           b+=purchasePrice[(int) j];
+           result=Math.max(result,(b-a));
             }
-        }
+
         String answer = String.valueOf(result);
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
         writer.write(answer);
