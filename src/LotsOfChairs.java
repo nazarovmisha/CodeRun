82bc6df4a5afb8b45bfd1a5c5aec71247996164a
Nazarov Mikhail <nazarovmisha@inbox.ru>
2024-09-29 11:36:22 +0300

    LotsOfChairs

--- src/LotsOfChairs.java
+++ src/LotsOfChairs.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.math.BigInteger;
 import java.util.Arrays;
 
 //https://coderun.yandex.ru/problem/trading-ya-intern/solutions/10010066-f431-0fbd-b073-8f82d8a4ca99?currentPage=2&groups=backend&pageSize=20&search=
@@ -10,26 +11,18 @@ public class LotsOfChairs {
         String salePriceString = reader.readLine();
         String purchasePriceString = reader.readLine();
         reader.close();
-        long[] sellersAndByers = Arrays.stream(byers.split(" ")).mapToLong(Integer::parseInt).toArray();
-        long[] salePrice = Arrays.stream(salePriceString.split(" ")).mapToLong(Integer::parseInt).toArray();
-        long[] purchasePrice = Arrays.stream(purchasePriceString.split(" ")).mapToLong(Integer::parseInt).toArray();
-        long sales;
-        if (sellersAndByers[0] < sellersAndByers[1]) {
-            sales = sellersAndByers[0];
-        } else {
-            sales = sellersAndByers[1];
-        }
+        int[] sellersAndByers = Arrays.stream(byers.split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] salePrice = Arrays.stream(salePriceString.split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] purchasePrice = Arrays.stream(purchasePriceString.split(" ")).mapToInt(Integer::parseInt).toArray();
+        int sales=Math.min(sellersAndByers[0],sellersAndByers[1]);
         Arrays.sort(salePrice);
         Arrays.sort(purchasePrice);
-        long result = 0l;
-        long a =0l;
-        long b =0l;
-        for (long i = 0l, j = sales-1l; i < sales; i++, j--) {
-           a+=salePrice[(int) i];
-           b+=purchasePrice[(int) j];
-           result=Math.max(result,(b-a));
+        BigInteger result=new BigInteger("0");
+        for (int i = 0, j = sales-1; i < sales; i++, j--) {
+         if ((purchasePrice[j]-salePrice[i])>0){
+             result=result.add(BigInteger.valueOf(purchasePrice[j] - salePrice[i]));
+         }
             }
-
         String answer = String.valueOf(result);
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
         writer.write(answer);
